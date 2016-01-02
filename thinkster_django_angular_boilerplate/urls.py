87c9f3b3f7c381bcd6da2c8801e8f9180f8b4979
Muhamad A <m@muhamad.me>
2016-01-02 18:35:48 +0200

    Add an API endpoint for LoginView

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 from rest_framework_nested import routers
 
 from thinkster_django_angular_boilerplate.views import IndexView
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -10,5 +10,6 @@ router.register(r'accounts', AccountViewSet)
 urlpatterns = patterns(
     '',
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
 )
