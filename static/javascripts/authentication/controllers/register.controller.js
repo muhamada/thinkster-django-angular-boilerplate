cef309589dd860d38adb13863899e13e150baf6e
Muhamad A <m@muhamad.me>
2016-01-02 21:20:55 +0200

    Update Authentication.register

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -36,11 +36,35 @@
 
     /**
     * @name register
-    * @desc Register a new user
-    * @memberOf thinkster.authentication.controllers.RegisterController
+    * @desc Try to register a new user
+    * @param {string} email The email entered by the user
+    * @param {string} password The password entered by the user
+    * @param {string} username The username entered by the user
+    * @returns {Promise}
+    * @memberOf thinkster.authentication.services.Authentication
     */
-    function register() {
-      Authentication.register(vm.email, vm.password, vm.username);
+    function register(email, password, username) {
+      return $http.post('/api/v1/accounts/', {
+        username: username,
+        password: password,
+        email: email
+      }).then(registerSuccessFn, registerErrorFn);
+
+      /**
+      * @name registerSuccessFn
+      * @desc Log the new user in
+      */
+      function registerSuccessFn(data, status, headers, config) {
+        Authentication.login(email, password);
+      }
+
+      /**
+      * @name registerErrorFn
+      * @desc Log "Epic failure!" to the console
+      */
+      function registerErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      }
     }
   }
 })();
