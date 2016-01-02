b860660fb904791d0d2698ab5ff7f8c6827edc78
Muhamad A <m@muhamad.me>
2016-01-02 18:31:06 +0200

    Make a view called LoginView in authentication/views.py

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,41 @@ from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
 
+import json
+
+from django.contrib.auth import authenticate, login
+
+from rest_framework import status, views
+from rest_framework.response import Response
+
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
