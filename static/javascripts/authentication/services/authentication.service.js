bb0307a0acc60d95aca81dd154f9aaa4b8b6982d
Muhamad A <m@muhamad.me>
2016-01-02 18:37:53 +0200

    Add a login method to your Authentication service

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -26,6 +26,7 @@
     * return it, leaving the details lower in the file.
     */
     var Authentication = {
+      login: login,
       register: register
     };
 
@@ -49,5 +50,20 @@
         email: email
       });
     }
+
+    /**
+     * @name login
+     * @desc Try to log in with email `email` and password `password`
+     * @param {string} email The email entered by the user
+     * @param {string} password The password entered by the user
+     * @returns {Promise}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+    function login(email, password) {
+      return $http.post('/api/v1/auth/login/', {
+        email: email, password: password
+      });
+    }
+
   }
 })();
