f2444791ba48133a7f7a87fb0a1146e932bd39e0
Muhamad A <m@muhamad.me>
2016-01-02 18:01:45 +0200

    import include module

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from rest_framework_nested import routers
 
 from thinkster_django_angular_boilerplate.views import IndexView
